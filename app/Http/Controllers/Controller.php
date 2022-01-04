cb780b6919fcc2c7364e5a0616c250720838cdcf
Professor93M <professor404@gmail.com>
2022-01-04 12:33:20 +0300

    user & items Controller with Routes

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,7 +6,9 @@ use App\Models\User;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\Redirect;
 use Inertia\Inertia;
 
 class Controller extends BaseController
@@ -20,9 +22,26 @@ class Controller extends BaseController
         ]);
     }
 
-    public function create()
+    public function add()
     {
         return Inertia::render('User/Add');
+    }
+
+    public function insert(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|unique:users,name',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|password|confirm',
+        ]);
+
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => $request->password,
+        ]);
+
+        return Redirect::route('dashboard')->with('success', 'تم الاضافة بنجاح');
     }
 
     public function edit($user)
@@ -30,6 +49,30 @@ class Controller extends BaseController
         return Inertia::render('User/Edit', [
             'user' => User::findOrFail($user)->with('image')->first()
         ]);
+    }
+
+    public function update(Request $request, $id){
+        $user = User::findOrFail($id);
+
+        $request->validate([
+            'name' => 'required|string|unique:users,name',
+            'email' => 'required|email|unique:users,email',
+        ]);
+
+        $user->update([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => $request->password,
+            'isAdmin' => $request->isAdmin,
+        ]);
+
+        return Redirect::route('dashboard')->with('success', 'تم التحديث بنجاح');
+    }
+
+    public function delete($user){
+        $user = User::findOrFail($user);
+        $user->delete();
+        return Redirect::route('dashboard')->with('success', 'تم الحذف بنجاح');
     }
 
 }
