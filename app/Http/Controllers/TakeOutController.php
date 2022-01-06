4ade1dbb6bf64ea9ea015dfbbdba94f077a4163b
Professor93M <professor404@gmail.com>
2022-01-06 19:43:04 +0300

    khalid ibn al waleed

--- app/Http/Controllers/TakeOutController.php
+++ app/Http/Controllers/TakeOutController.php
@@ -35,6 +35,9 @@ class TakeOutController extends Controller
     }
 
     public function refresh(){
-        return redirect()->route('takeout.index');
+        // return redirect()->route('takeout.index');
+        return Inertia::render('TakeOut/Checkout', [
+            'items' => Items::all(),
+        ]);
     }
 }
