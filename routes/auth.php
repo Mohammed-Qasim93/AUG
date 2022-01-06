e7d480b5ae04a56963fc0d366473e64b7d177860
Professor93M <professor404@gmail.com>
2022-01-06 18:59:05 +0300

    items

--- routes/auth.php
+++ routes/auth.php
@@ -35,7 +35,7 @@ Route::resource('logs', LogsController::class)->middleware('auth');
 
 Route::middleware('auth')->group(function () {
     Route::get('/takeout', [TakeOutController::class, 'index'])->name('takeout.index');               // Index
-    Route::post('/checkout', [TakeOutController::class, 'checkout'])->name('takeout.checkout');               // Index
+    Route::get('/checkout', [TakeOutController::class, 'checkout'])->name('takeout.checkout');               // Index
     // Route::get('/user/{id}/edit', [Controller::class, 'edit'])->name('user.edit');        // Edit
     // Route::put('/user/{id}', [Controller::class, 'update'])->name('user.update');        // Update
     // Route::delete('/user/{id}', [Controller::class, 'delete'])->name('user.delelte');    // Delete
